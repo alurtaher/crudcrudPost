dc56f6cb6cc8ed901e60225ce7f5bf362fccf506
alurtaher <117931556+alurtaher@users.noreply.github.com>
2023-12-10 18:57:11 +0530

    Second commit Get Request from Crud Crud

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 document.getElementById('submitButton').addEventListener('click', function() {
     saveAndDisplay();
+    displayUserInfo();
 });
 
 function saveAndDisplay() {
-    var name = document.getElementById('myname').value;
-    var fatherName = document.getElementById('fname').value;
-    var motherName = document.getElementById('mname').value;
-    var mobileNumber = document.getElementById('cno').value;
-    var dateOfBirth = document.getElementById('dob').value;
-    var gender = document.querySelector('input[name="myGender"]:checked').id;
-    var selectedCar = document.getElementById('car').value;
+    var nname = document.getElementById('myname').value;
+    var nmobileNumber = document.getElementById('cno').value;
 
     var userInfo = {
-        name: name,
-        fatherName: fatherName,
-        motherName: motherName,
-        mobileNumber: mobileNumber,
-        dateOfBirth: dateOfBirth,
-        gender: gender,
-        selectedCar: selectedCar
+        name: nname,
+        mobileNumber: nmobileNumber,
     };
 
    // localStorage.setItem('userInfo', JSON.stringify(userInfo));
@@ -33,17 +24,27 @@ function saveAndDisplay() {
     })
 }
 
-function displayUserInfo() {
-    var storedUserInfo = localStorage.getItem('userInfo');
-    var userInfo = JSON.parse(storedUserInfo);
+window.addEventListener("DOMContentLoaded",()=>{
+    
+    axios.get("https://crudcrud.com/api/c5ea4d9d37c647d2b6bab84a0fffa56e/appointmentData")
+    .then((response)=>{
+        for(var i=0;i<response.data.length;i++){
+            console.log(response.data[i]);
+            displayUserInfo(response.data[i]);
+
+        }
+        console.log(response)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+function displayUserInfo(userInfo) {
+   // var storedUserInfo = localStorage.getItem('userInfo');
+    //var userInfo = JSON.parse(storedUserInfo);
 
     var displayDiv = document.getElementById('userInfoDisplay');
-    displayDiv.innerHTML = `<h3>User Information:</h3>
-                           <p>Name: ${userInfo.name}</p>
-                           <p>Father Name: ${userInfo.fatherName}</p>
-                           <p>Mother Name: ${userInfo.motherName}</p>
-                           <p>Mobile Number: ${userInfo.mobileNumber}</p>
-                           <p>Date of Birth: ${userInfo.dateOfBirth}</p>
-                           <p>Gender: ${userInfo.gender}</p>
-                           <p>Selected Car: ${userInfo.selectedCar}</p>`;
+    displayDiv.innerHTML = `<p>Name: ${userInfo.name}</p>
+                           <p>Mobile Number: ${userInfo.mobileNumber}</p>`;
 }
